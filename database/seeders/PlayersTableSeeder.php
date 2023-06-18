a12157848240698b469f7b0b9a7d11205a6ce3fc
davidhalasz <davidhalasz22@gmail.com>
2023-06-18 19:49:35 +0200

    update seeder

--- database/seeders/PlayersTableSeeder.php
+++ database/seeders/PlayersTableSeeder.php
@@ -17,98 +17,98 @@ class PlayersTableSeeder extends Seeder
             'name' => 'Player 1',
             'avatar' => 'players/avatar1.jpg',
             'position' => 'Forward',
-            'team_id' => 4,
+            'team_id' => 1,
     ]);
     Player::create([
             'name' => 'Player 2',
             'avatar' => 'players/avatar2.jpg',
             'position' => 'Midfielder',
-            'team_id' => 4,
+            'team_id' => 1,
     ]);
     Player::create([
             'name' => 'Player 3',
             'avatar' => 'players/avatar3.jpg',
             'position' => 'Forward',
-            'team_id' => 4,
+            'team_id' => 1,
     ]);
     Player::create([
             'name' => 'Player 4',
             'avatar' => 'players/avatar4.jpg',
             'position' => 'Midfielder',
-            'team_id' => 4,
+            'team_id' => 1,
     ]);
     Player::create([
             'name' => 'Player 5',
             'avatar' => 'players/avatar5.jpg',
             'position' => 'Forward',
-            'team_id' => 4,
+            'team_id' => 1,
     ]);
     Player::create([
             'name' => 'Player 6',
             'avatar' => 'players/avatar6.jpg',
             'position' => 'Midfielder',
-            'team_id' => 4,
+            'team_id' => 1,
     ]);
     Player::create([
             'name' => 'Player 7',
             'avatar' => 'players/avatar7.jpg',
             'position' => 'Forward',
-            'team_id' => 4,
+            'team_id' => 1,
     ]);
     Player::create([
             'name' => 'Player 8',
             'avatar' => 'players/avatar8.jpg',
             'position' => 'Midfielder',
-            'team_id' => 4,
+            'team_id' => 1,
     ]);
 
     Player::create([
         'name' => 'Player 11',
         'avatar' => 'players/avatar8.jpg',
         'position' => 'Forward',
-        'team_id' => 5,
+        'team_id' => 2,
     ]);
     Player::create([
             'name' => 'Player 12',
             'avatar' => 'players/avatar7.jpg',
             'position' => 'Midfielder',
-            'team_id' => 5,
+            'team_id' => 2,
     ]);
     Player::create([
             'name' => 'Player 13',
             'avatar' => 'players/avatar6.jpg',
             'position' => 'Forward',
-            'team_id' => 5,
+            'team_id' => 2,
     ]);
     Player::create([
             'name' => 'Player 14',
             'avatar' => 'players/avatar5.jpg',
             'position' => 'Midfielder',
-            'team_id' => 5,
+            'team_id' => 2,
     ]);
     Player::create([
             'name' => 'Player 15',
             'avatar' => 'players/avatar4.jpg',
             'position' => 'Forward',
-            'team_id' => 5,
+            'team_id' => 2,
     ]);
     Player::create([
             'name' => 'Player 16',
             'avatar' => 'players/avatar3.jpg',
             'position' => 'Midfielder',
-            'team_id' => 5,
+            'team_id' => 2,
     ]);
     Player::create([
             'name' => 'Player 17',
             'avatar' => 'players/avatar2.jpg',
             'position' => 'Forward',
-            'team_id' => 5,
+            'team_id' => 2,
     ]);
     Player::create([
             'name' => 'Player 18',
             'avatar' => 'players/avatar1.jpg',
             'position' => 'Midfielder',
-            'team_id' => 5,
+            'team_id' => 2,
     ]);
     }
 }
